52c82b90c8c914642d4260088f46cab58b9d7e93
r0binsonCrusoe <cwc.cal@gmail.com>
2025-07-12 19:14:37 -0400

    Precip comparison and finish trails processing

--- trails_processing_bbox.py
+++ trails_processing_bbox.py
@@ -3,89 +3,84 @@ import pandas as pd
 import geopandas as gpd
 from pathlib import Path
 
-data_dir = Path("data/trails")
-results = []
-bboxes = [
-    [-101.0000, 28.0000, -95.0000, 32.0000],
-    [-101.0000, 32.0000, -95.0000, 37.0000],
-    [-95, 25, -89, 31],
-    [-95, 31, -89, 37],
-]
+# data_dir = Path("data/trails")
+# results = []
+file = "data/trails/socal-latest.osm.pbf"
 
-
-trail_filter_mtb = {"route": ["mtb"]}
 
 ### Loop 1 MTB trails filter 1
-for file in data_dir.glob("*.osm.pbf"):
-    for box in bboxes:
-        print(f"processing {file}, {box}")
-        osm = OSM(str(file), bounding_box=box)
+# for file in data_dir.glob("*.osm.pbf"):
 
-        trails_mtb = osm.get_data_by_custom_criteria(
-            custom_filter=trail_filter_mtb,
-            filter_type="keep",
-        )
+# trail_filter_mtb = {"route": ["mtb"]}
 
-        if trails_mtb is not None and not trails_mtb.empty:
-            results.append(trails_mtb)
-            print(f"Appended results with mtb trails from {file}, {box}")
+# print(f"processing {file}")
+# osm = OSM(str(file))
 
-        ### Loop 2 MTB trails filter 2
-        ##for file in data_dir.glob("*.osm.pbf"):
-        print(f"processing filter 2 on {file}, {box}")
+# trails_mtb = osm.get_data_by_custom_criteria(
+#     custom_filter=trail_filter_mtb,
+#     filter_type="keep",
+# )
 
-        # osm = OSM(str(file))
+# if trails_mtb is not None and not trails_mtb.empty:
+#     results.append(trails_mtb)
+#     print(f"Appended results with mtb trails from {file}")
 
-        trails_mtb_2 = osm.get_data_by_custom_criteria(
-            custom_filter={},
-            osm_keys_to_keep=["mtb:scale"],
-            filter_type="keep",
-        )
+### Loop 2 MTB trails filter 2
+# ##for file in data_dir.glob("*.osm.pbf"):
+# print(f"processing filter 2 on {file}")
 
-        if trails_mtb_2 is not None and not trails_mtb_2.empty:
-            results.append(trails_mtb_2)
-            print(f"Appended results of filter 2 with mtb trails from {file}, {box}")
+# osm = OSM(str(file))
 
-        ### Loop 3 - Hiking trails filter 1
-        # for file in data_dir.glob("*.osm.pbf"):
-        print(f"processing filter 3 on {file}, {box}")
-        # osm = OSM(str(file))
+# trails_mtb_2 = osm.get_data_by_custom_criteria(
+#     custom_filter={},
+#     osm_keys_to_keep=["mtb:scale"],
+#     filter_type="keep",
+# )
 
-        trail_filter_hiking_1 = {}
+# if trails_mtb_2 is not None and not trails_mtb_2.empty:
+#     results.append(trails_mtb_2)
+#     print(f"Appended results of filter 2 with mtb trails from {file}")
 
-        trails_hiking_1 = osm.get_data_by_custom_criteria(
-            osm_keys_to_keep=["sac_scale", "osmc:symbol"],
-            custom_filter=trail_filter_hiking_1,
-            filter_type="keep",
-        )
+### Loop 3 - Hiking trails filter 1
+# for file in data_dir.glob("*.osm.pbf"):
+# print(f"processing filter 3 on {file}")
+# osm = OSM(str(file))
 
-        if trails_hiking_1 is not None and not trails_hiking_1.empty:
-            results.append(trails_hiking_1)
-            print(f"Appended results of filter 3 with hiking trails from {file}, {box}")
+# trail_filter_hiking_1 = {}
 
-        ### Loop 4 - Hiking trails filter 2
-        # for file in data_dir.glob("*.osm.pbf"):
-        print(f"processing filter 4 on {file}, {box}")
+# trails_hiking_1 = osm.get_data_by_custom_criteria(
+#     osm_keys_to_keep=["sac_scale", "osmc:symbol"],
+#     custom_filter=trail_filter_hiking_1,
+#     filter_type="keep",
+# )
 
-        # osm = OSM(str(file))
+# if trails_hiking_1 is not None and not trails_hiking_1.empty:
+#     results.append(trails_hiking_1)
+#     print(f"Appended results of filter 3 with hiking trails from {file}")
 
-        trail_filter_hiking_2 = {"route": ["hiking"]}
+# ### Loop 4 - Hiking trails filter 2
+# # for file in data_dir.glob("*.osm.pbf"):
+print(f"processing filter 4 on {file}")
 
-        trails_hiking_2 = osm.get_data_by_custom_criteria(
-            custom_filter=trail_filter_hiking_2,
-            filter_type="keep",
-        )
+osm = OSM(str(file))
 
-        if trails_hiking_2 is not None and not trails_hiking_2.empty:
-            results.append(trails_hiking_2)
-            print(f"Appended results of filter 4 with hiking trails from {file}, {box}")
+trail_filter_hiking_2 = {"route": ["hiking"]}
 
-all_trails = gpd.GeoDataFrame(pd.concat(results, ignore_index=True), crs="EPSG:4326")
+trails_hiking_2 = osm.get_data_by_custom_criteria(
+    custom_filter=trail_filter_hiking_2,
+    filter_type="keep",
+)
 
-all_trails.drop_duplicates()
+# if trails_hiking_2 is not None and not trails_hiking_2.empty:
+#     results.append(trails_hiking_2)
+#     print(f"Appended results of filter 4 with hiking trails from {file}")
 
-all_trails_final = all_trails[["id", "tags", "osm_type", "geometry"]]
+# all_trails = gpd.GeoDataFrame(results, ignore_index=True, crs="EPSG:4326")
 
-all_trails_final.to_file(
-    "data/processed/us_trails.gpkg", driver="GPKG", mode="a"
+# all_trails.drop_duplicates()
+print("processing geodataframe and appending gpkg file")
+alltrails = trails_hiking_2[["id", "tags", "osm_type", "geometry"]]
+
+alltrails.to_file(
+    "data/processed/alltrails.gpkg", driver="GPKG", mode="a"
 )  # add mode='a' to next batches to append the file.
